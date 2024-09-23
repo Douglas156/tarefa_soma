6fd2cdf9883214fd0d605e696afbfc87467a1cf1
Douglas <douglas0fac@gmail.com>
2024-09-22 21:03:21 -0300

    Criação do código para testar valor "soma"

---
+++ tarefa_soma/src/tarefa_soma/Main.java
@@ -0,0 +1,14 @@
+package tarefa_soma;
+import tarefa_soma.Metodo;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Metodo executar = new Metodo();
+		
+		System.out.println(executar.somaValores());
+		
+	}
+
+}
---
+++ tarefa_soma/src/tarefa_soma/Metodo.java
@@ -0,0 +1,24 @@
+package tarefa_soma;
+
+public class Metodo {
+	
+	
+	
+	
+	
+	
+	
+	public int somaValores() {
+		int indice = 12;
+		int soma = 0;
+		int k = 1;
+		while (k < indice) {
+			k++;
+			soma = soma + k;
+		}
+		return soma;
+	}
+	
+	
+
+}
